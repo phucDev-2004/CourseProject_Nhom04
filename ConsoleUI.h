2cb5136866ca505d27b6a4aedcf3e10ba8bf20d9
manhthe-jn06 <tmt.manhthe@gmail.com>
2026-06-29 19:11:36 +0700

    Create ConsoleUI.h

---
+++ ConsoleUI.h
@@ -0,0 +1,13 @@
+#ifndef CONSOLE_UI_H
+#define CONSOLE_UI_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void hienThiMenu();
+void hienThiTrangThaiThangMay(int tangHienTai, string huongDiChuyen, int taiTrongHienTai, int taiTrongToiDa);
+void hienThiDanhSachKhach(int soLuongKhachCho, int soLuongKhachTrongThang);
+
+#endif
